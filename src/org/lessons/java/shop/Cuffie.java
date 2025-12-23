e214788c125f7dd20bb2d5cd41a6357e541f18a6
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-12-23 12:32:39 +0100

    aggiunta sottoclasse Cuffie

---
+++ src/org/lessons/java/shop/Cuffie.java
@@ -0,0 +1,31 @@
+package org.lessons.java.shop;
+
+public class Cuffie extends Prodotto {
+
+    protected String colore;
+    protected boolean isWireless;
+
+    public Cuffie(String nome, String descrizione, float prezzo, int iva, String colore, boolean isWireless){
+        super(nome, descrizione, prezzo, iva);
+        this.colore = colore;
+        this.isWireless = isWireless;
+    }
+
+    // getter 
+    public String getColore(){
+        return colore;
+    }
+
+    public boolean getIsWireless(){
+        return isWireless;
+    }
+
+    // setter
+    public void setColore(String colore){
+        this.colore = colore;
+    }
+
+    public void setIsWireless(boolean isWireless){
+        this.isWireless = isWireless;
+    }
+}
--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -5,12 +5,15 @@ public class Main {
 
         Smartphone nothing = new Smartphone("3A", "telefono con ia", 350, 22, 1234, 128);
 
+        System.out.println("Smartphone ");
+
         System.out.println("codice: " + nothing.getCodice());
         System.out.println("nome: " + nothing.getNome());
         System.out.println("imei: " + nothing.getImei());
         System.out.println("prezzo con iva: " + nothing.getPrezzoConIva());
 
         System.out.println("-------------- ");
+        System.out.println("Televisore ");
 
         Televisori samsung = new Televisori("TV", "tv in 4k", 999, 22, "106x93x52", true);
 
@@ -20,8 +23,14 @@ public class Main {
         System.out.println("prezzo con iva: " + samsung.getPrezzoConIva());
 
         System.out.println("-------------- ");
+        System.out.println("Cuffie ");
 
+        Cuffie hyperx = new Cuffie("cloud stinger", "cuffie da gaming", 70, 22, "nero", false);
 
+        System.out.println("codice: " + hyperx.getCodice());
+        System.out.println("colore: " + hyperx.getColore());
+        System.out.println("wireles: " + hyperx.getIsWireless());
+        System.out.println("prezzo con iva: " + hyperx.getPrezzoConIva());
 
     }
 }
