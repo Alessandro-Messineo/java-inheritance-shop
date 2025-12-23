3b81ba48abf4bdd43863ba9860d792debea89328
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-12-23 12:21:15 +0100

    aggiunta sottoclase Televisori

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -8,5 +8,20 @@ public class Main {
         System.out.println("codice: " + nothing.getCodice());
         System.out.println("nome: " + nothing.getNome());
         System.out.println("imei: " + nothing.getImei());
+        System.out.println("prezzo con iva: " + nothing.getPrezzoConIva());
+
+        System.out.println("-------------- ");
+
+        Televisori samsung = new Televisori("TV", "tv in 4k", 999, 22, "106x93x52", true);
+
+        System.out.println("codice: " + samsung.getCodice());
+        System.out.println("dimensioni: " + samsung.getDimensioni());
+        System.out.println("smart: " + samsung.getIsSmart());
+        System.out.println("prezzo con iva: " + samsung.getPrezzoConIva());
+
+        System.out.println("-------------- ");
+
+
+
     }
 }
---
+++ src/org/lessons/java/shop/Televisori.java
@@ -0,0 +1,32 @@
+package org.lessons.java.shop;
+
+public class Televisori extends Prodotto{
+
+    protected String dimensioni;
+    protected boolean isSmart;
+
+    public Televisori(String nome, String descrizione, float prezzo, int iva, String dimensioni, boolean isSmart){
+        super(nome, descrizione, prezzo, iva);
+        this.dimensioni = dimensioni;
+        this.isSmart = isSmart;
+    }
+
+    // getter
+    public String getDimensioni(){
+        return dimensioni;
+    }
+
+    public boolean getIsSmart(){
+        return isSmart;
+    }
+
+    // setter
+    public void setDimensioni(String dimensioni){
+        this.dimensioni = dimensioni;
+    }
+
+    public void setIsSmart(boolean isSmart){
+        this.isSmart = isSmart;
+    }
+
+}
